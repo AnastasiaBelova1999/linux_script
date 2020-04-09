323d791243b3b87ed291951f26ed684e583babbd
AnastasiaBelova1999 <43291963+AnastasiaBelova1999@users.noreply.github.com>
2020-04-09 22:10:51 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+# Hello World Program in Bash Shell
+
+num=`free -mh`
+echo "$( tput setaf 2)
+Рады приветствовать Вас.
+Надеюсь вы не сломаете ничего за первые 2 минуты)0)
+
+Running Processes..: `ps ax | wc -l | tr -d " "`
+
+$num
+
+$USER
+
+$(tput sgr0)"
